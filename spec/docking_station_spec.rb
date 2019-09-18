dcbd16cc467321d2054240128dd0923378be3720
John Dennehy <john.dennehy@gmail.com>
2019-09-18 16:45:14 +0100

    11 - amends test for attribute reader

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,25 +2,28 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike).with(0).argument }
+  
   it 'release good bike' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
   it do
-    expect(subject.dock(Bike.new)).to respond(:bike)
+    station = DockingStation.new
+    bike = station.release_bike
+    station.dock(bike)
+    expect(station.bike).to eq bike
   end
 end
 
 
-=begin
-  it 'release good bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  # it 'release good bike' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 
-=begin
-  it do
-    expect(subject.dock(Bike.new)).to respond_to(:bike)
-  end
-  =end
+  # it do
+  #   expect(subject.dock(Bike.new)).to respond_to(:bike)
+  # end
