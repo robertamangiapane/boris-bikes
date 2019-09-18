15732166fbf2098684983e345159e2cfb9c8bfe3
robertamangiapane <roberta.mangiapane@gmail.com>
2019-09-18 14:14:20 +0100

    Step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:release_bike).with(0).argument }
   it 'release good bike' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it {is_expected.to respond_to(:return_bike).with(1).argument}
+
+
 end
